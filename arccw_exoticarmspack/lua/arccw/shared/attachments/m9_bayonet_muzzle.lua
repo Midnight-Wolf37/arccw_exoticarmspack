1fa7bd7f73ee641c942a18baef8e665835502668
Midnight_Wolf <79667509+Midnight-Wolf37@users.noreply.github.com>
2021-02-25 15:43:09 -0600

    Added attachments from proposed funky pack

---
+++ arccw_exoticarmspack/lua/arccw/shared/attachments/m9_bayonet_muzzle.lua
@@ -0,0 +1,29 @@
+att.PrintName = "M9 Bayonet"
+att.Icon = Material("entities/acwatt_go_muzz_bayonet.png", "mips smooth")
+att.Description = "Standard bayonet of the United States Army. Allows for more efficient melee combat."
+
+att.SortOrder = 1
+
+att.Desc_Pros = {
+}
+att.Desc_Cons = {
+}
+att.AutoStats = true
+att.Slot = "muzzle"
+
+att.Model = "models/weapons/arccw_go/atts/bayonet.mdl"
+att.OffsetAng = Angle(0, 0, -90)
+
+att.Add_BarrelLength = 3
+
+att.Mult_SightTime = 1.05
+
+att.Mult_MoveSpeed = 0.95
+
+att.Mult_MeleeDamage = 2
+
+att.Add_BarrelLength = 3
+
+att.Mult_MeleeTime = 0.9
+
+att.Add_MeleeRange = 16
