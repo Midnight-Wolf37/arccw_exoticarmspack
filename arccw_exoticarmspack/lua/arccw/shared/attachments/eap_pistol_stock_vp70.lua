6e0d8295efcbc5c4439191790cd528d049adf0cd
Midnight-Wolf37 <79667509+Midnight-Wolf37@users.noreply.github.com>
2021-02-25 15:11:53 -0600

    Uploaded Lebedev & VP70 files

---
+++ arccw_exoticarmspack/lua/arccw/shared/attachments/eap_pistol_stock_vp70.lua
@@ -0,0 +1,31 @@
+att.PrintName = "VP70 Pistol Stock"
+att.Icon = Material("entities/acwatt_go_stock_pistol_bt.png", "mips smooth")
+att.Description = "Thumbhole stock from the VP70 machine pistol that adds a 3-round burst and drastically increases fire rate."
+att.Desc_Pros = {
+}
+att.Desc_Cons = {
+}
+att.AutoStats = true
+att.Slot = "go_stock_pistol_bt"
+
+att.Model = "models/weapons/arccw/eap/atts/pistol_stock_vp70.mdl"
+
+att.Override_Firemodes = {
+	{
+		Mode = -3,
+	},
+    {
+        Mode = 1,
+    },
+    {
+        Mode = 0
+    }
+}
+
+att.Mult_RPM = 1.5
+att.Mult_SightTime = 1.3
+att.Mult_Recoil = 0.85
+att.Mult_MoveDispersion = 0.85
+att.Mult_MoveSpeed = 0.95
+att.Mult_DrawSpeed = 1.5
+
---
+++ arccw_exoticarmspack/lua/autorun/arccw_exoticarms_sounds.lua
@@ -0,0 +1,48 @@
+sound.Add({
+	name = 			"eap_lebby.magout",
+	channel = 		CHAN_AUTO,
+	volume = 		1.0,
+	sound = 			"weapons/arccw/eap/lebby/magout.wav"
+})
+
+sound.Add({
+	name = 			"eap_lebby.magin",
+	channel = 		CHAN_AUTO,
+	volume = 		1.0,
+	sound = 			"weapons/arccw/eap/lebby/magin.wav"
+})
+
+sound.Add({
+	name = 			"eap_lebby.draw",
+	channel = 		CHAN_AUTO,
+	volume = 		1.0,
+	sound = 			"weapons/arccw/eap/lebby/draw.wav"
+})
+
+sound.Add({
+	name = 			"eap_lebby.slideback",
+	channel = 		CHAN_AUTO,
+	volume = 		1.0,
+	sound = 			"weapons/arccw/eap/lebby/slideback.wav"
+})
+
+sound.Add({
+	name = 			"eap_lebby.sliderelease",
+	channel = 		CHAN_AUTO,
+	volume = 		1.0,
+	sound = 			"weapons/arccw/eap/lebby/sliderelease.wav"
+})
+
+sound.Add({
+	name = 			"eap_lebby.disappear",
+	channel = 		CHAN_AUTO,
+	volume = 		1.0,
+	sound = 			"weapons/arccw/eap/lebby/disappear.wav"
+})
+
+sound.Add({
+	name = 			"eap_lebby.reveal",
+	channel = 		CHAN_AUTO,
+	volume = 		1.0,
+	sound = 			"weapons/arccw/eap/lebby/reveal.wav"
+})
