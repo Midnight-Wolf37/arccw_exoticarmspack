cd67e36e9a124b8013da6852f49ba52ff5975918
Midnight_Wolf <79667509+Midnight-Wolf37@users.noreply.github.com>
2021-02-26 10:32:37 -0600

    Coyote fully implemented

--- arccw_exoticarmspack/lua/arccw/shared/attachments/eap_optic_coyote.lua
+++ arccw_exoticarmspack/lua/arccw/shared/attachments/eap_optic_coyote.lua
@@ -1,8 +1,8 @@
 att.PrintName = "Coyote (Holo)"
-att.Icon = Material("entities/arccw_mifl_fas2_optic_pso1.png", "smooth")
-att.Description = "Chinese reflex sight identifiable by the fang-like hood over the lens."
+att.Icon = Material("entities/eap_optic_coyote.png", "smooth")
+att.Description = "Chinese reflex sight identifiable by the fang-like hood over the lens. Unobstructive compared to other reflex sights."
 
-att.SortOrder = 4
+att.SortOrder = 1
 
 att.Desc_Pros = {
 	"You might know what the text on the frame means.",
@@ -15,16 +15,14 @@ att.AutoStats = true
 att.Slot = "optic"
 
 att.Model = "models/weapons/arccw/eap/atts/eap_optic_coyote.mdl"
-att.ModelScale = Vector(1.5, 1.5, 1.5)
-att.ModelOffset = Vector(0, 0, 0)
-att.OffsetAng = Angle(-90, 0, 0)
+att.ModelOffset = Vector(0, 0, -0.08)
 att.HolosightPiece = "models/weapons/arccw/eap/atts/eap_optic_coyote_hsp.mdl"
 
 att.AdditionalSights = {
     {
-        Pos = Vector(0, 2, -2),
-        Ang = Angle(0, 90, 0),
-        Magnification = 1.1,
+        Pos = Vector(0, 4, -1.375),
+        Ang = Angle(0, 0, 0),
+        Magnification = 1,
         ScrollFunc = ArcCW.SCROLL_NONE
     },
 }
@@ -32,14 +30,12 @@ att.Holosight = true
 att.HolosightMagnification = 1
 att.HolosightReticle = Material("eap_scopes/coyote.png", "mips smooth")
 att.HolosightNoFlare = true
-att.HolosightSize = 15
+att.HolosightSize = 5
 att.HolosightBone = "holosight"
 att.Colorable = true
 
 att.ColorOptionsTable = {
-    Color(255, 50, 50),
-    Color(50, 255, 50),
-	Color(128,0,128)
+    Color(255, 50, 50)
 }
 
-att.Mult_SightTime = 1.03
+att.Mult_SightTime = 1.01
