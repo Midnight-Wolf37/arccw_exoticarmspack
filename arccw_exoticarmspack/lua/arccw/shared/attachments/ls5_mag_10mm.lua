243d37c695ac06628355b05eccfa89ff458ea2eb
Midnight_Wolf <79667509+Midnight-Wolf37@users.noreply.github.com>
2021-02-28 00:40:38 -0600

    Added new attachments

---
+++ arccw_exoticarmspack/lua/arccw/shared/attachments/ls5_mag_10mm.lua
@@ -0,0 +1,20 @@
+att.PrintName = "20-Round 10mm Auto"
+att.Icon = Material("entities/arccw_mifl_fas2_mp5_mag_10mm.png", "mips smooth")
+att.Description = "Convert weapon to fire the more powerful 10mm Auto with increased damage and less load. The straight magazine is ever so slightly heavier."
+att.SortOrder = 20
+att.Desc_Pros = {
+}
+att.Desc_Cons = {
+}
+att.AutoStats = true
+att.Slot = "ls5_mag"
+
+att.Override_ClipSize = 20
+
+att.Mult_Damage = 1.15
+att.Mult_DamageMin = 1.15
+
+att.Mult_Recoil = 1.1
+att.Mult_RPM = 0.9
+
+att.Mult_ReloadTime = 1.05
