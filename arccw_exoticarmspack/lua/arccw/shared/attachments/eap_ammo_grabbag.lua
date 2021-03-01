8852ee61ce3a954005f1570e7f80defc3d25157f
Midnight_Wolf <79667509+Midnight-Wolf37@users.noreply.github.com>
2021-03-01 15:02:42 -0600

    Delete eap_ammo_grabbag.lua

--- arccw_exoticarmspack/lua/arccw/shared/attachments/eap_ammo_grabbag.lua
+++
@@ -1,8 +0,0 @@
-att.PrintName = "Grab Bag"
-att.Icon = nil
-att.Description = "Your mags have been loaded with a randomized assortment of ammo types. Each shot is a mystery."
-att.Desc_Pros = { "Completely random effect on each shot." }
-att.Desc_Cons = { "Effect can be good or bad." }
-att.Slot = "go_ammo"
-att.TargetSpecificWeapon = "" -- ALWAYS make this attachment available on a specific weapon
-att.TargetSpecificSlot = 0 -- on this specific slot
