83b08a2468948364504cbf354b1210b05225580d
Midnight_Wolf <79667509+Midnight-Wolf37@users.noreply.github.com>
2021-03-01 01:33:06 -0600

    added credits

---
+++ arccw_exoticarmspack/lua/arccw/shared/documentation/credits.lua
@@ -0,0 +1,22 @@
+//
+
+Well, with all the assets here drawn from all corners of the interwebs, might as well jot them down somewhere."
+Here's everything and everyone that has contributed to this pack, gun by gun:
+
+"PL-15 Lebedev"
+Original CS:GO addon - Pavlienda
+Sounds - Navaro & zen1th
+
+"Jianshe CS/LS5"
+Original Model - 
+Baked normals & Normal maps - 
+Diffuse map - Midnight_Wolf
+Wooden furniture - Matias Hollman https://devassets.com/assets/modern-weapons/
+SD handguard - slogkot
+Sounds - Navaro
+
+"Attachments"
+Boomermobile - Lonewolfie
+Pistol & SMG mags - slogkot
+
+Magpul mags - 
