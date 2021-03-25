c0ed9faf64ba6fadd2111b1d4d18b3491a360ef3
Midnight_Wolf <79667509+Midnight-Wolf37@users.noreply.github.com>
2021-03-24 21:21:13 -0500

    implemented thumbhole stock

---
+++ lua/arccw/shared/attachments/eap_stg_stock_thumb.lua
@@ -0,0 +1,16 @@
+att.PrintName = "Thumbhole Stock"
+att.Icon = nil
+att.Description = "Lengthened thumbhole stock from a sporting rifle. Greatly reduces recoil, but is considerably more cumbersome."
+att.SortOrder = 15
+att.Desc_Pros = {
+}
+att.Desc_Cons = {
+}
+att.AutoStats = true
+att.Slot = "eap_stg_stock"
+
+att.Mult_SightTime = 1.25
+att.Mult_Recoil = 0.65
+att.Mult_RecoilSide = 0.75
+att.Mult_SightedSpeedMult = 0.7
+att.Mult_MoveDispersion = 1.25
