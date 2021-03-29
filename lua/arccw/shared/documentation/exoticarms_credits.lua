50b0b6140af6c42cb534da5cc73f442949566a2e
Midnight_Wolf <79667509+Midnight-Wolf37@users.noreply.github.com>
2021-03-29 14:07:35 -0500

    Create exoticarms_credits.lua

---
+++ lua/arccw/shared/documentation/exoticarms_credits.lua
@@ -0,0 +1,50 @@
+All animations, model edits, and Lua coding done by Midnight_Wolf.
+
+PL-15 Lebedev / P17-K
+Model - Pavlienda
+Sounds - Navaro & MagmaCow
+
+Jianshe CS/LS5 / JC-LGM
+Model - sharksplitter
+Textures - KingstonCartel (normal map) & Midnight_Wolf (diffuse map)
+Sounds - Navaro
+Wooden Furniture - Matias Hollmann
+SD Handguard & Extra Magazines - Firearms Source team & Slogkot
+
+SITES Spectre M4 / Modello 4 Phantom
+Model - RedRogueXIII
+Textures - 4Echo
+Sounds - KillerExe_01
+Rail mount - Overkill Software
+Extra Magazines - Firearms Source team & Slogkot
+
+AEK-971 / A-545
+Model & Textures - adept1993 & Street-you
+Sounds - Navaro & rzen1th
+Rail mount - Overkill Software
+7.62 steel mags & Wooden stock - New World Interactive
+7.62 poly mags - Valve
+5.45 30 & 45 round mags - Paul Yakushev
+AMD-65 parts - 
+SR-3M parts - 
+AS Val parts -
+
+FMG-9 / SMG9 ARES
+Model & Textures - 
+Sounds - Navaro, NightmareMutant, & Magmacow
+
+VP70 / Pistole M70
+Model -
+Textures -
+Sounds -
+
+Sturmgewehr 44 / Maschinenkarabiner 44H
+Model & Textures - Mr. Why, 4Echo, & Infinity Ward
+Sounds - Navaro, NightmareMutant, & Magmacow
+SD Handguard - Firearms Source team & Slogkot
+Holger Handguard - Kally
+MP40 Parts - Raynox3d
+
+Credit to Slogkot, Dr Gordon Freeman, & Infinity ward for that drum mag you see on almost every gun.
+
+Special thanks to everyone else who has helped make this pack a possibility.
