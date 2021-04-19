9ae87dc560c1443c0a880806da5c5c478cf3b7c9
Midnight_Wolf <79667509+Midnight-Wolf37@users.noreply.github.com>
2021-04-19 13:04:12 -0500

    uploading stuff from my school computer

---
+++ lua/arccw/shared/attachments/eap_charm_donut.lua
@@ -0,0 +1,24 @@
+att.PrintName = "Pink Frosted Sprinke Donut"
+att.Icon = nil
+att.Description = "A donut so powerful, it reigns above all other foods that attempt to challenge it in arbitrary fights."
+
+att.Desc_Pros = {
+    "5% buff to all stats"
+}
+att.Desc_Cons = {
+}
+att.AutoStats = true
+att.Slot = "charm"
+
+att.Free = true
+
+att.Model = "models/weapons/arccw/atts/charmbase.mdl"
+
+att.DroppedModel = "models/Items/BoxSRounds.mdl"
+
+att.Charm = true
+att.CharmModel = "models/props_junk/PopCan01a.mdl"
+att.CharmAtt = "Charm"
+att.CharmScale = Vector(0.25, 0.25, 0.25)
+att.CharmOffset = Vector(0, -0.25, 0)
+att.CharmAngle = Angle(90, 0, 90)
---
+++ lua/arccw/shared/attachments/eap_groza_barrel_gripped.lua
@@ -0,0 +1,14 @@
+att.PrintName = "590mm SAW Barrel"
+att.Icon = nil
+att.Description = "Extended barrel that makes the rifle into a squad automatic weapon. Also includes integrated bipod."
+att.Desc_Pros = {
+}
+att.Desc_Cons = {}
+att.Slot = "eap_groza_barrel"
+
+
+
+
+
+att.Mult_SightTime = 1.1
+
